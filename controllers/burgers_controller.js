1bc1a15a394259907d7b95e2790437e1eb07c755
Isaac Kwon <you@example.com>
2017-10-02 21:31:32 -0700

    post

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,29 +3,37 @@ const db = require("../models");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", (req, res) => {
   db.burgers.findAll({}).then((data) => {
     let hbsObject = {
       burger: data
     };
-    res.render("index", hbsObject);
+    return res.render("index", hbsObject);
   });
 });
-//
+
 router.post("/", (req, res) => {
   db.burgers.create({
     burger_name: req.body.burgers
   }).then((result) => {
     res.json(result);
   });
+  res.redirect("/");
 });
-//
-// router.put("/:id", (req, res) => {
-//   var condition = "id = " + req.params.id;
-//   burger.update({
-//     devoured: req.body.devoured
-//   }, condition, () => {
-//     res.redirect("/");
-//   });
-// });
-//
+
+router.put("/burgers/:id", (req, res) => {
+  db.burgers.update({
+    devoured: req.body.devoured
+  }, {
+    where: {
+      id: req.body.id
+    }
+  }).then((result) => {
+    res.json(result);
+  });
+});
+
 module.exports = router;
